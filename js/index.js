a553aff6ec2fe07b298c7fbcb0c5bd6d6c98139e
Dima Jarm <d.jarmolin@gmail.com>
2018-08-03 09:28:00 +0000

    format code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 let n;
 do {
     n = parseInt(prompt('Привіт! 😺 Я кіт Фібоначчі і знаю всі його числа. Перевіримо? Введи порядковий номер: '));
-} while(isNaN(n))
+} while (isNaN(n))
 
 function fibonachi(n) {
     let one = 1;
     let two = 2;
-    for (let i=3; i<=n-1; i++) {
-        let three= one + two;
+    for (let i = 3; i <= n - 1; i++) {
+        let three = one + two;
         one = two;
         two = three;
     }
